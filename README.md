82d7ffdba3e616164af99b376010996dbe66b6ef
neil-vqa <nvq.alino@gmail.com>
2024-11-08 22:20:43 +0800

    documentation

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # kuki
 
-Simple Speech-to-Text App
+Simple offline speech-to-text desktop app
+
+Slack? Teams? Sometimes, typing can be inconvenient when you just want to get your idea across to your team immediately. With Kuki, take away the hassle of typing what you want to say.
+
+![logo](/assets/kuki-demo.png)
+
+![logo](/assets/kuki-logo.png)
--- gui.py
+++ gui.py
@@ -10,6 +10,11 @@ is_dev = dev_var in ["1", "True", "true"]
 
 if __name__ == "__main__":
     webview.create_window(
-        "Kuki | Simple Speech-to-Text App", app, height=460, width=360, http_port=8118
+        "Kuki | Simple Speech-to-Text App",
+        app,
+        height=460,
+        width=360,
+        http_port=8118,
+        background_color="#2a2a2a",
     )
     webview.start(debug=is_dev)
